97b285fd91c8abe174ed635eacb81407f5c47bd3
Tyler Jarvis <tyler.jarvis@developersinstitute.co.nz>
2023-06-21 10:20:47 +1200

    Finished the tablet styling and started the desktop styling. Also changed product items into grids so that they stretch to the same size when the screen size is adjusted.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,11 @@ const Footer = () => {
       <p className="copyright">Copyright, Jap Snack, by Tyler Jarvis 2023.</p>
       <div className="contact-container">
         <h4 className="contact-title">Contact</h4>
-        <p className="contact-details phone">Phone Number</p>
-        <p className="contact-details email">Email</p>
-        <p className="contact-details address">Address</p>
+        <div className="contact-details-container">
+          <p className="contact-details phone">Phone Number</p>
+          <p className="contact-details email">Email</p>
+          <p className="contact-details address">Address</p>
+        </div>
       </div>
     </div>
   );
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,22 +1,20 @@
 const Product = ({ src, name, description, price, onCheckout }) => {
   return (
-    <div className="products-container">
-      <li className="product-item">
-        <img className="product-image" src={src} alt={name} />
-        <h3 className="product-name">{name}</h3>
-        <p className="product-description">{description}</p>
-        <p className="product-price">
-          $
-          {price.toLocaleString("en-NZ", {
-            maximumFractionDigits: 2,
-            minimumFractionDigits: 2,
-          })}
-        </p>
-        <button className="buy-now" onClick={onCheckout}>
-          Buy Now
-        </button>
-      </li>
-    </div>
+    <li className="product-item">
+      <img className="product-image" src={src} alt={name} />
+      <h3 className="product-name">{name}</h3>
+      <p className="product-description">{description}</p>
+      <p className="product-price">
+        $
+        {price.toLocaleString("en-NZ", {
+          maximumFractionDigits: 2,
+          minimumFractionDigits: 2,
+        })}
+      </p>
+      <button className="buy-now" onClick={onCheckout}>
+        Buy Now
+      </button>
+    </li>
   );
 };
 
